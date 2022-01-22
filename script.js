d3a93652fcf451263f7ff2800667a96dc996e3aa
signan <signmeup132@gmail.com>
2022-01-22 19:43:14 +0300

    refactor 'playRound' function to accept arguments and return values instead of console.logging

--- script.js
+++ script.js
@@ -4,27 +4,27 @@ function computerPlay() {
     return randomChoice;
 }
 
-function roundOfRPS() {
-    let computerSelection = computerPlay();
-    let playerSelection = window.prompt("enter a choice").toLowerCase();
-    console.log(playerSelection);
-    console.log(computerSelection);
-
-    // if equal "tie"
+function playRound(playerSelection, computerSelection) {
     if (computerSelection === playerSelection) {
-        console.log("It's a tie");
+        return "It's a tie";
     }
     else if ((computerSelection === "paper" && playerSelection === "rock") ||
             (computerSelection === "scissors" && playerSelection === "paper") ||
             (computerSelection === "rock" && playerSelection === "scissors")) {
-        console.log("Computer wins!!!");
+        return "Computer wins!!!";
     }
     else if ((computerSelection === "scissors" && playerSelection === "rock") ||
             (computerSelection === "rock" && playerSelection === "paper") ||
             (computerSelection === "paper" && playerSelection === "scissors")) {
-        console.log("Player wins!!!");
+        return "Player wins!!!";
         }
     else {
-        console.log("not a choice");
+        return "not a choice";
     }
 }
+
+
+let computerSelection = computerPlay();
+let playerSelection = window.prompt("enter a choice").toLowerCase();
+
+console.log(playRound(computerSelection, playerSelection))
