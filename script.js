65cb4dd35210c434cd1b18ec1dd88cfdd0249681
signan <signmeup132@gmail.com>
2022-01-25 19:27:21 +0300

    create game() function

--- script.js
+++ script.js
@@ -4,27 +4,69 @@ function computerPlay() {
     return randomChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(computerSelection, playerSelection) {
+    //a tie
     if (computerSelection === playerSelection) {
-        return "It's a tie";
+        return 0;
     }
+    //pc wins
     else if ((computerSelection === "paper" && playerSelection === "rock") ||
             (computerSelection === "scissors" && playerSelection === "paper") ||
             (computerSelection === "rock" && playerSelection === "scissors")) {
-        return "Computer wins!!!";
+        return 1;
     }
+    //player wins
     else if ((computerSelection === "scissors" && playerSelection === "rock") ||
             (computerSelection === "rock" && playerSelection === "paper") ||
             (computerSelection === "paper" && playerSelection === "scissors")) {
-        return "Player wins!!!";
+        return 2;
         }
+    //note a valid choice
     else {
-        return "not a choice";
+        return -1;
     }
 }
 
+function declareWinner(computerScore, playerScore) {
+    if (computerScore > playerScore) {
+        return "Computer wins with score of " + computerScore + " against " + playerScore;
+    }
+    else {
+        return "Player wins with score of " + playerScore + " against " + computerScore;
+    }
+}
 
-let computerSelection = computerPlay();
-let playerSelection = window.prompt("enter a choice").toLowerCase();
+function game() {
+    let computerScore = 0;
+    let playerScore = 0;
+    for (let i=1; i <= 5; i++) {
+        let computerSelection = computerPlay();
+        let playerSelection = window.prompt("enter a choice").toLowerCase();
+        let roundResult = playRound(computerSelection, playerSelection)
+        console.log("roundResult " + roundResult);
+        switch (roundResult) {
+            case 0:
+                console.log("It's a tie. both are " + playerSelection);
+                i--;
+                break;
+            case 1:
+                console.log("PC Wins!!! with " + computerSelection + " against " + playerSelection);
+                computerScore++;
+                break;
+            case 2:
+                console.log("Player Wins!!! with " + playerSelection + " against " + computerSelection);
+                playerScore++;
+                break;
+            case -1:
+                console.log(playerSelection + " is not a valid choice. play again.");
+                i--;
+                break;
+        }
 
-console.log(playRound(computerSelection, playerSelection))
+        console.log("round " + i + "\npc: " + computerScore + "\nplayer: " + playerScore);
+    }
+    console.log(declareWinner(computerScore, playerScore));
+}
+
+
+game();
